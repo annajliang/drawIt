e043bf8efa85cc14a7a6ffaa329f694049284ea8
anna liang <annajliang@gmail.com>
2020-04-28 12:56:11 -0400

    routing issue fixed thanks to talia

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 // import words from './data/words';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header';
 import Canvas from './components/Canvas';
 import Footer from './components/Footer';
+import Guess from './components/Guess'
+import Gallery from './components/Gallery'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <main>
-          <Canvas />
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            {/* <Canvas /> */}
+            <Route exact path="/" component={Canvas} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/guess" component={Guess} />
+          </main>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import Guess from './Guess'
-import Gallery from './Gallery'
-import App from "../App";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+// import Guess from './Guess'
+// import Gallery from './Gallery'
+// import App from "../App";
 
 class Navbar extends Component {
     render() {
         return(
-            <Router>
+            <>
                 <nav>
                     <ul className="navbarLinks">
                         <li>
-                            <Link to="/app" className="home">Home</Link>
-                            <Route path="/app" component={App} />
+                            <Link to="/" className="home">Home</Link>
+                            {/* <Route path="/app" component={App} /> */}
                         </li>
                         <li>
                             <Link to="/gallery">Gallery</Link>
-                            <Route path="/gallery" component={Gallery} />
+                            {/* <Route path="/gallery" component={Gallery} /> */}
                         </li>
                         <li>
                             <Link to="/guess">Guess</Link>
-                            <Route path="/guess" component={Guess} />
+                            {/* <Route path="/guess" component={Guess} /> */}
                         </li>
                     </ul>
                 </nav>
-            </Router>
+            </>
         )
     }
 }
